37d5b7b51fff7905b37b6d9b0c55dd579dc3e2f8
Rone Shender Loza Aliaga <rone@b89.io>
2021-11-24 13:36:23 -0500

    Initial Commit

---
+++ TMDb/TMDbApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDbApp.swift
+//  TMDb
+//
+//  Created by Rone Shender on 24/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TMDbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
